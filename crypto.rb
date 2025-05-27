8f24b05db69d341b39ede05aa147c5873b372f49
Murilo Zanardo <97256579+mrh4t@users.noreply.github.com>
2025-05-27 13:19:49 -0300

    Add files via upload

---
+++ crypto.rb
@@ -0,0 +1,62 @@
+require 'net/http'
+require 'json'
+
+class Crypto
+  def initialize(cryptocurrencies, currencies)
+    @cryptocurrencies = cryptocurrencies.flatten.map(&:upcase)
+    @currencies = currencies.flatten.map(&:upcase)
+  end
+
+  def monitor(time)
+    loop do
+      system("clear") || system("cls")
+      puts "Atualizado em: #{Time.now.strftime('%H:%M:%S')}"
+      actual_price
+      sleep(time)
+    end
+  end
+
+  def actual_price
+    @cryptocurrencies.each do |crypto|
+      prices = fetch_crypto_price(crypto)
+      @currencies.each do |currency|
+        puts "O preço atual da #{crypto} em #{currency} é #{currency} #{prices[currency]}"
+      end
+      puts "\n\n"
+    end
+  end
+
+  private
+
+  def fetch_crypto_price(crypto)
+    url = URI("https://min-api.cryptocompare.com/data/price?fsym=#{crypto}&tsyms=#{@currencies.join(',')}")
+    response = Net::HTTP.get(url)
+    JSON.parse(response)
+  end
+end
+system("cls")
+puts "⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣴⣶⣾⣿⣿⣿⣿⣷⣶⣦⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⣠⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⣄⠀⠀⠀⠀⠀
+⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⠀⠀⠀
+⠀⠀⣴⣿⣿⣿⣿⣿⣿⣿⠟⠿⠿⡿⠀⢰⣿⠁⢈⣿⣿⣿⣿⣿⣿⣿⣿⣦⠀⠀
+⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣤⣄⠀⠀⠀⠈⠉⠀⠸⠿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀
+⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⠀⠀⢠⣶⣶⣤⡀⠀⠈⢻⣿⣿⣿⣿⣿⣿⣿⡆
+⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠼⣿⣿⡿⠃⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣷
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀⢀⣀⣀⠀⠀⠀⠀⢴⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⢿⣿⣿⣿⣿⣿⣿⣿⢿⣿⠁⠀⠀⣼⣿⣿⣿⣦⠀⠀⠈⢻⣿⣿⣿⣿⣿⣿⣿⡿
+⠸⣿⣿⣿⣿⣿⣿⣏⠀⠀⠀⠀⠀⠛⠛⠿⠟⠋⠀⠀⠀⣾⣿⣿⣿⣿⣿⣿⣿⠇
+⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⣤⡄⠀⣀⣀⣀⣀⣠⣾⣿⣿⣿⣿⣿⣿⣿⡟⠀
+⠀⠀⠻⣿⣿⣿⣿⣿⣿⣿⣄⣰⣿⠁⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠀⠀
+⠀⠀⠀⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⠀⠀⠀
+⠀⠀⠀⠀⠀⠙⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠋⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠻⠿⢿⣿⣿⣿⣿⡿⠿⠟⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀"
+
+puts "Crypto Monitor by: Murilo Zanardo"
+puts "Digite as criptomoedas que deseja monitorar, separadas por vírgula (EX: BTC, ETH, XMR):"
+cryptos = gets.chomp.split(',').map(&:strip)
+puts "Digite as moedas que deseja monitorar, separadas por vírgula (EX: USD, BRL, EUR):"
+currencies = gets.chomp.split(',').map(&:strip)
+puts "Digite o intervalo entre cada atualização (em segundos, exemplo: 5)"
+time = gets.chomp.to_i
+Crypto.new(cryptos, currencies).monitor(time)
+
